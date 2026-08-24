47c80ea9903c8f89e2c9250acaf10910a11f774b
Farhan Noor Abir <fnoor.abir@gmail.com>
2026-08-24 16:00:16 +0100

    Update changelog-section.tsx

--- apps/web/src/components/pages/home/changelog-section.tsx
+++ apps/web/src/components/pages/home/changelog-section.tsx
@@ -1,14 +1,10 @@
-"use client";
-
 import Link from "next/link";
 import { CardVersion } from "@repo/ui";
-import { useAuth } from "@/contexts/AuthContext";
 
 export default function ChangelogSection() {
-  const { isAdmin } = useAuth();
   return (
     <Link href="/changelog" className="w-full">
-      <CardVersion isAdmin={isAdmin} clickable />
+      <CardVersion clickable />
     </Link>
   );
 }
