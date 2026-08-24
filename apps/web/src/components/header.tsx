c020b7f5d5ff80203d732a499939de15cfbeb81f
Farhan Noor Abir <fnoor.abir@gmail.com>
2026-08-24 15:20:04 +0100

    Add logout button and hide breadcrumb when user not available

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,16 +1,33 @@
 "use client";
-
+import { MdLogout } from "react-icons/md";
+import { signOut } from "@repo/firebase";
+import { Button } from "@repo/ui";
 import Breadcrumb from "./breadcrumb";
 import ThemeToggle from "./theme-toggle";
 import { useBreadcrumb } from "@/contexts/BreadcrumbContext";
+import { useAuth } from "@/contexts/AuthContext";
 
 export default function Header() {
   const { items } = useBreadcrumb();
+  const { user } = useAuth();
+
+  const handleSignOut = async () => {
+    await signOut();
+  };
 
   return (
     <header className="w-full flex flex-row items-center justify-between py-2 px-2 lg:px-4">
-      <Breadcrumb items={items} />
+      {user ? <Breadcrumb items={items} /> : <div />}
       <div className="flex flex-row items-center gap-2">
+        {user && (
+          <Button
+            onClick={handleSignOut}
+            ariaLabel="Logout Button"
+            label="Logout"
+            variant="danger"
+            Icon={<MdLogout />}
+          />
+        )}
         <ThemeToggle />
       </div>
     </header>
