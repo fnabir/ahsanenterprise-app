aa022f46fd6a687f8bdfb1f12dfb5f67eb62b609
Farhan Noor Abir <fnoor.abir@gmail.com>
2026-09-04 19:40:47 +0100

    Use the store and selectors for importer info data

--- apps/web/src/components/pages/info-importer-section.tsx
+++ apps/web/src/components/pages/info-importer-section.tsx
@@ -1,43 +1,48 @@
 "use client";
 
-import { getDatabaseReference } from "@repo/firebase";
-import { useList } from "react-firebase-hooks/database";
 import { CardInfo, CardInfoImporter, Skeleton } from "@repo/ui";
 import { FaInfoCircle } from "react-icons/fa";
+import {
+  useImporterLoading,
+  useImporterError,
+  useImporterData,
+} from "@repo/core";
+import Loading from "@/components/loading";
 
 export default function InfoImporterSection() {
-  const [data, loading, error] = useList(getDatabaseReference("info/importer"));
+  const loading = useImporterLoading();
+  const error = useImporterError();
+  const data = useImporterData();
+
+  if (loading) return <Loading />;
+
+  if (error)
+    return (
+      <div className="h-full grow flex flex-col items-center justify-center gap-1">
+        <FaInfoCircle size={24} className="text-danger mb-1" />
+        <div className="font-semibold">
+          Error occured loading importer information.
+        </div>
+        <div className="text-muted">{error}</div>
+      </div>
+    );
+
+  if (!data || Object.keys(data).length !== 0)
+    return (
+      <div className="h-full grow flex flex-col items-center justify-center gap-1">
+        <FaInfoCircle size={24} className="text-muted mb-1" />
+        <div className="font-semibold">No data found.</div>
+        <div className="text-muted">
+          No information available for importers.
+        </div>
+      </div>
+    );
 
   return (
     <div className="grid grid-cols-5 gap-2 lg:gap-4">
-      {loading ? (
-        Array.from({ length: 4 }).map((_, i) => (
-          <Skeleton key={i} className="col-span-1">
-            <div className="w-1/4 h-4" />
-            <div className="w-3/5 h-10" />
-            <div className="w-2/5 h-3" />
-            <div className="w-4/7 h-3" />
-          </Skeleton>
-        ))
-      ) : error ? (
-        <CardInfo
-          title="Error"
-          details={`Error getting importer informations. ${error?.message ?? ""}`}
-          Icon={<FaInfoCircle size={24} className="text-danger" />}
-          className="mx-2 lg:mx-4 my-2"
-        />
-      ) : !data?.length ? (
-        <CardInfo
-          title="No data found"
-          details="No information available for importers."
-          Icon={<FaInfoCircle size={24} />}
-          className="mx-2 lg:mx-4 my-2"
-        />
-      ) : (
-        data?.map((item, i) => (
-          <CardInfoImporter key={i} id={item.key!} data={item.val()} />
-        ))
-      )}
+      {Object.entries(data ?? {}).map(([key, value], i) => (
+        <CardInfoImporter key={i} id={key} data={value} />
+      ))}
     </div>
   );
 }
