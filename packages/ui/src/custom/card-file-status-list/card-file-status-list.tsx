ecb1be428ad6546b7754b67f91a7a7b5f578d63e
Farhan Noor Abir <fnoor.abir@gmail.com>
2026-09-04 19:43:23 +0100

    Add the dropdown menu to change file status from dashboard

--- packages/ui/src/custom/card-file-status-list/card-file-status-list.tsx
+++ packages/ui/src/custom/card-file-status-list/card-file-status-list.tsx
@@ -1,8 +1,35 @@
+"use client";
+
 import { MdMoreVert } from "react-icons/md";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+} from "../../core/dropdown-menu";
 import type { GroupedFileItem } from "@repo/types";
-import { Button } from "../..";
+import { useState, useEffect } from "react";
+import { updateFileStatus } from "@repo/firebase";
+import { FILE_STATUS_OPTIONS } from "@repo/core";
+import { Button } from "../../core";
 
 export function CardFileStatusList({ file }: { file: GroupedFileItem }) {
+  const [status, setStatus] = useState<string>(file.data.status ?? "New");
+
+  useEffect(() => {
+    setStatus(file.data.status ?? "New");
+  }, [file.data.status]);
+
+  const onChangeStatus = async (nextStatus: string) => {
+    if (nextStatus === status) return;
+    setStatus(nextStatus);
+
+    await updateFileStatus(file.fileNo, file.year, status, nextStatus);
+  };
+
   return (
     <li className="flex items-center justify-between gap-2 px-2 pb-1 text-xs">
       <div>
@@ -19,11 +46,35 @@ export function CardFileStatusList({ file }: { file: GroupedFileItem }) {
         )}
       </div>
 
-      <Button
-        variant="outline"
-        Icon={<MdMoreVert size={16} />}
-        className="p-1!"
-      />
+      <DropdownMenu>
+        <DropdownMenuTrigger
+          render={
+            <Button
+              Icon={<MdMoreVert size={18} />}
+              className="p-1!"
+              variant="muted"
+            />
+          }
+        />
+        <DropdownMenuContent className="w-42">
+          <DropdownMenuGroup>
+            <DropdownMenuLabel>File Status</DropdownMenuLabel>
+            <DropdownMenuRadioGroup
+              value={status}
+              onValueChange={onChangeStatus}
+            >
+              {FILE_STATUS_OPTIONS.map((statusOption) => (
+                <DropdownMenuRadioItem
+                  key={statusOption.value}
+                  value={statusOption.value}
+                >
+                  {statusOption.label}
+                </DropdownMenuRadioItem>
+              ))}
+            </DropdownMenuRadioGroup>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </li>
   );
 }
