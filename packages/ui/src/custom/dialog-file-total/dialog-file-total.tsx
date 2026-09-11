87f6d5578a629544d08309000c533c144d9b4b5e
Farhan Noor Abir <fnoor.abir@gmail.com>
2026-09-11 11:35:56 +0100

    Add importer info dependency of default value

--- packages/ui/src/custom/dialog-file-total/dialog-file-total.tsx
+++ packages/ui/src/custom/dialog-file-total/dialog-file-total.tsx
@@ -57,7 +57,7 @@ export function DialogFileTotal({
       paid: data?.paid != null ? String(data.paid) : "",
       remarks: data?.remarks ?? "",
     };
-  }, [data]);
+  }, [data, importerInfo]);
 
   const {
     control,
