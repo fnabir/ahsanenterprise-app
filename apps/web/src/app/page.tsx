9e890f7b3ce8db3ef1b9c3aa074c146c1aca1c24
Farhan Noor Abir <fnoor.abir@gmail.com>
2026-09-19 23:13:50 +0100

    Allow scrolling the main and side pane independently

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -7,14 +7,14 @@ import StatusOverviewSection from "@/components/pages/home/status-overview-secti
 
 export default function Home() {
   return (
-    <div className="grow flex flex-col lg:flex-row divide-x-2 gap-2 px-2 lg:px-4">
-      <div className="grow flex flex-col gap-4 pr-2 lg:pr-4 divide-y-2 pt-1.5 lg:pt-3 pb-2 lg:pb-4">
+    <div className="grow h-full flex flex-col lg:flex-row divide-x-2 gap-2 px-2 lg:px-4">
+      <div className="grow min-h-0 overflow-auto flex flex-col gap-4 pr-2 lg:pr-4 divide-y-2 pt-1.5 lg:pt-3 pb-2 lg:pb-4">
         <BalanceSection />
         <FilesSection />
       </div>
 
       <hr />
-      <div className="w-full lg:w-1/5 shrink-0 flex flex-col gap-4 pt-1.5 lg:pt-3 pb-2 lg:pb-4">
+      <div className="min-h-0 overflow-auto w-full lg:w-1/5 shrink-0 flex flex-col gap-4 pt-1.5 lg:pt-3 pb-2 lg:pb-4">
         <ChangelogSection />
         <StatusOverviewSection />
         <RequisitionSection />
